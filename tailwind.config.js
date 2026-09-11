7bb1a3ff4bc10df27919f934168230a2605d7b6a
TSISabit <tariksaidulislam@gmail.com>
2026-09-12 01:30:44 +0600

    Added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'brand-gradient': 'linear-gradient(to right, #f97316, #ec4899, #8b5cf6)',
+      },
+    },
+  },
+  plugins: [],
+}
